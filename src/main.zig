9d72c1fc030ec35b4284be68bcfd63604069efab
MrNorwood <[email protected]>
2026-02-18 04:01:39 +0100

    plugin loading

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const wp = @import("wp");
 const glib = @import("glib");
 const go = @import("gobject");
+const gio = @import("gio");
 
 const WpError = error{ConnectionFailed};
 
@@ -14,11 +15,13 @@ pub const ZjContext = struct {
     core: ?*wp.Core,
     loop: ?*glib.MainLoop,
     om: ?*wp.ObjectManager,
+    pending_pugins: u8,
     pub fn init() ZjContext {
         return ZjContext{
             .core = null,
             .loop = null,
             .om = null,
+            .pending_pugins = 0,
         };
     }
 
@@ -32,10 +35,33 @@ pub const ZjContext = struct {
 
         std.debug.print("\nExiting...", .{});
     }
+
+    pub fn onAddPlugin(_: ?*go.Object, res: ?*gio.AsyncResult, p_input: ?*anyopaque) callconv(.c) void {
+        const zj: *ZjContext = @ptrCast(@alignCast(p_input.?));
+        var err: ?*glib.Error = null;
+        if (zj.core.?.loadComponentFinish(res.?, &err) <= 0) {
+            if (err) |e| {
+                std.debug.print("Load failed: {s}\n", .{e.f_message orelse "null"});
+                defer err.?.free();
+            }
+            return;
+        }
+        zj.pending_pugins -= 1;
+        if (zj.pending_pugins == 0) {
+            const mixer = wp.Plugin.find(zj.core.?, "mixer-api");
+            const gobj: *go.Object = @ptrCast(mixer.?);
+            gobj.set("scale", @as(c_int, 1), @as(?*anyopaque, null));
+
+            zj.core.?.installObjectManager(zj.om.?);
+        }
+    }
 };
 
+//Basically null for anything that requires GType, TODO: Fix up bindings
+const unull = @as(usize, 0);
+
 fn onSigInt(p_data: ?*anyopaque) callconv(.c) c_int {
-    const zj: *ZjContext = @ptrCast(@alignCast(p_data orelse return -1));
+    const zj: *ZjContext = @ptrCast(@alignCast(p_data.?));
 
     std.debug.print("\nSIGINT recieved, stopping loop", .{});
     zj.loop.?.quit();
@@ -55,14 +81,18 @@ pub fn main() !void {
     zj.om = wp.ObjectManager.new();
 
     const client_type = wp.Client.getGObjectType();
-    zj.om.?.addInterest(client_type, @as(usize, 0));
+    zj.om.?.addInterest(client_type, unull);
     zj.om.?.requestObjectFeatures(client_type, 31);
-
-    zj.core.?.installObjectManager(zj.om.?);
 
     if (zj.core.?.connect() < 0) {
         return WpError.ConnectionFailed;
     }
+    zj.pending_pugins += 1;
+    zj.core.?.loadComponent("libwireplumber-module-default-nodes-api", "module", null, null, null, &ZjContext.onAddPlugin, &zj);
+    zj.pending_pugins += 1;
+    zj.core.?.loadComponent("libwireplumber-module-mixer-api", "module", null, null, null, &ZjContext.onAddPlugin, &zj);
+
+    //zj.core.?.loadComponent("libwireplumber-module-mixer-api", "module", null);
 
     std.debug.print("Successfully connected to WirePlumber!\n", .{});
     _ = glib.unixSignalAdd(2, &onSigInt, &zj);
